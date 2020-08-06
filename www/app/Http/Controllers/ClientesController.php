9aef6ce7b3e78deb13d4542674fb49ee58a1ba1d
Jadson Freitas <jadarau@gmail.com>
2020-08-06 00:24:08 -0300

    06082020 2
    06082020 2

---
+++ www/app/Http/Controllers/ClientesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Clientes;
+
+class ClientesController extends Controller
+{
+    //
+    public function cadastrar(Request $request){
+        $empresa = $request->empresa;
+        $cnpj = $request->cnpj;
+        $telefone = $request->tel;
+        $responsavel = $request->responsavel;
+        $email = $request->email;
+
+        $salvar = new Clientes();
+
+        // echo $empresa;
+        // die();
+
+        if($salvar->cadastrar($empresa, $cnpj, $telefone, $responsavel, $email)){
+            echo "<script>OI</script>";
+            return redirect()->route('home')->with('msg','Salvo com sucesso!');
+        }else{
+            echo "<script>NAO</script>";
+            return redirect()->route('home')->with('msg','Ocorreu um erro! Tente novamente.');
+        }
+    }
+}
--- www/app/Http/Controllers/UserController.php
+++ www/app/Http/Controllers/UserController.php
@@ -3,29 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Clientes;
 
 class UserController extends Controller
 {
     //
-    public function cadastrar(Request $request){
-        $empresa = $request->empresa;
-        $cnpj = $request->cnpj;
-        $telefone = $request->tel;
-        $responsavel = $request->responsavel;
-        $email = $request->email;
-
-        $salvar = new Clientes();
-
-        // echo $empresa;
-        // die();
-
-        if($salvar->cadastrar($empresa, $cnpj, $telefone, $responsavel, $email)){
-            echo "<script>OI</script>";
-            return redirect()->route('home')->with('msg','Salvo com sucesso!');
-        }else{
-            echo "<script>NAO</script>";
-            return redirect()->route('home')->with('msg','Ocorreu um erro! Tente novamente.');
-        }
-    }
+   
 }
