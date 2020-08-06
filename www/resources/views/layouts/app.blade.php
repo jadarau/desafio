ee945a32fc7e99e07e80eb0c3ebf0a7a9a4a3513
Jadson Freitas <jadarau@gmail.com>
2020-08-06 01:06:25 -0300

    06082020 4
    06082020 4

--- www/resources/views/layouts/app.blade.php
+++ www/resources/views/layouts/app.blade.php
@@ -40,6 +40,10 @@
         #tela_busca{
             display:none;
         }
+        label{
+            color:black;
+            font-weight: bold;
+        }
     </style>
 
 </head>
@@ -99,7 +103,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" style="color:black" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
